2319b0cc2e59c1b37369787151738deeed46a8d5
liangxuanZ <2551316709@qq.com>
2020-12-26 18:53:51 +0800

    1.上传Excel 2.用户密码对比

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("请输入用户名：");
+        Scanner sc=new Scanner (System.in);
+        String username=sc.next();
+        System.out.println("你输入的用户名：" + username);
+        System.out.println("请输入密码：");
+        String password=sc.next();
+        System.out.println("你输入的密码是：" + password);
+        File file=new File ("C:\\Users\\Admin\\Desktop\\zlx.xlsx");
+        ReadExcel readExcel=new ReadExcel();
+        User users[]=readExcel.readExcel(file);
+        for (int i=0;i<users.length;i++){
+            if (username.equals(users[i].getUsername())&&password.equals(users[i].getPassword())){
+                System.out.println("登录成功！");
+                break;
+
+            }else {
+                System.out.println("登录失败！");
+            }
+        }
+
+
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,13 +1,38 @@
 public class User {
-    public void setUsername(String value) {
+    private String Username;
+    private String Password;
+    private String Address;
+    private String phone;
+
+    public String getUsername() {
+        return Username;
     }
 
-    public void setPassword(String value) {
+    public void setUsername(String username) {
+        Username = username;
     }
 
-    public void setAddress(String value) {
+    public String getPassword() {
+        return Password;
     }
 
-    public void setPhone(String value) {
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
